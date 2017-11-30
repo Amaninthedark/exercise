a66af066cbfa0c52e6bdb232d1b2f29776b82b76
Amaninthedark <741977480@qq.com>
2017-12-01 00:08:28 +0800

    12-1

--- WEB/Lib/Action/UserAction.class.php
+++ WEB/Lib/Action/UserAction.class.php
@@ -5,9 +5,20 @@
 		//注册
 		public function register(){
 
-			    $data['phone']  =$_GET['phone_data'];
+				$phone =$_GET['phone_data'];
+				$password=md5($_GET['password_data']);
+
+				//判断手机号码是否存在
+				$arr=M('user')->where("`phone`='$phone'")->find();
+				if($arr){
+					echo -1;
+					exit;
+				}
+
+			    $data['phone']  =$phone;
 			     $data['nicename']  ='中国移动';
-			     $data['password']  =md5($_GET['password_data']);
+			     $data['password']  =$password;
+			     $data['time']=time();
 			    $re = M('user')->add($data);
 			    echo $re;
 		} 
