5454ac1914603a9f7052236f8dff0bf69a9acf2a
Amaninthedark <741977480@qq.com>
2017-12-03 00:41:42 +0800

    12-3

--- WEB/Lib/Action/UserAction.class.php
+++ WEB/Lib/Action/UserAction.class.php
@@ -13,7 +13,7 @@
 				if($arr){
 					echo -1;
 					exit;
-				}
+				} 
 
 			    $data['phone']  =$phone;
 			     $data['nicename']  ='中国移动';
@@ -22,5 +22,32 @@
 			    $re = M('user')->add($data);
 			    echo $re;
 		} 
-	}
-?> 
+
+		//登录
+		public function login(){
+			$phone =$_GET['phone_data'];
+				$password=md5($_GET['password_data']);
+
+				//判断手机号码是否存在
+				$user=M('user')->where("`phone`='$phone'")->find();
+				if($user==''){
+					echo -1; 
+					exit; 
+				}
+				else{
+					if($password==$user['password']){
+						echo 1;
+					}else{
+						echo 0; 
+					} 
+				}
+
+			}
+		//获取用户信息
+		public function get_user_info(){
+				$phone =$_GET['phone_data']; 
+			$arr=M('user')->where("`phone`='$phone'")->find();
+			echo json_encode($arr);
+		} 
+	} 
+?>   
